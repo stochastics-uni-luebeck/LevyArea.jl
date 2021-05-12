8cd44d7703339971070491fd653b7cab1de0b038
Felix Kastner <kastner.felix@gmail.com>
2021-05-12 17:48:51 +0200

    use BenchmarkTools in test and output n and cost;

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,31 @@
 using IteratedIntegrals
-using Test
+using Test, BenchmarkTools
 import Random
-import Random: randn
 import LinearAlgebra: diag
 
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 1
 Random.seed!(638278)
 
 @testset "Iterated Integrals" begin
-#    @testset "Expected Output m=5" begin
-#        W = randn(5)
-#        Ints = IteratedIntegrals.simdoubleintegrals_n(W,2) # 2 approximation terms
-#        expected_output = [0.418259  0.229801    1.42757  -0.374705  -0.304829
-#                          -0.765741 -0.4218     -0.436137 -0.216972   0.180284
-#                          -0.417082  0.141253   -0.222006 -0.133748   0.0398915
-#                          0.701012   0.121748    0.313288 -0.471011  -0.0607856
-#                          -0.320829  0.00229782 -0.38414  -0.0503793 -0.393427]
-#        @test Ints ≈ expected_output atol=1e-5
-#    end
     @testset "m=1, h=$h" for h in rand(5)
         W = √h * randn()
-        @time Ints = simiterintegrals(W, h, h)
+        Ints = simiterintegrals(W, h, h^(3/2))
         @test Ints == 0.5W^2 - 0.5h
     end
-    h = 0.01
-    println("h = $h")
-    @testset "Alg $alg: Diagonal m=$i" for alg ∈ IteratedIntegrals.ITER_INT_ALGS, i in [1;50:50:500]
-        W = √h * randn(i)
-        print("Alg $alg: Diagonal m=$i ")
-        @time Ints = simiterintegrals(W, h, h^(3/2), alg=alg) # stepsize h
+
+    #trigger compilation
+    for alg ∈ IteratedIntegrals.ITER_INT_ALGS
+        simiterintegrals(randn(2),0.1,0.1, alg=alg)
+    end
+    
+    h = 0.0001
+    ϵ = h^(3/2)
+    println("h = $h, ϵ = $ϵ")
+    @testset "Alg $alg: m=$m" for alg ∈ IteratedIntegrals.ITER_INT_ALGS, m in [2;10;50;100;500]
+        W = √h * randn(m)
+        print("Alg $alg: m=$m n=$(terms_needed(m,h,ϵ,alg,MaxL2())) cost=$(IteratedIntegrals.effective_cost(m,h,ϵ,alg,MaxL2()))")
+        @btime simiterintegrals($W, $h, $ϵ, alg=$alg) # stepsize h
+        Ints = simiterintegrals(W, h, ϵ, alg=alg) # stepsize h
         @test diag(Ints) ≈ 0.5*W.^2 .- 0.5h
     end
 end
