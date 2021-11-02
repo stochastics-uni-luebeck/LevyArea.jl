7c393bd971700c4382caa72797c3462ab499fc33
Felix Kastner <kastner.felix@gmail.com>
2021-11-02 12:19:11 +0100

    add method for scalar Q-Wiener processes

--- src/simiterintegrals.jl
+++ src/simiterintegrals.jl
@@ -115,3 +115,16 @@ The parameter `eps` (as well as all additional keyword arguments) has no effect 
 to provide the same interface as the multidimensional version.
 """
 simiterintegrals(W::Real, h::Real, eps::Real=0.0; ito_correction=true, kwargs...) = ito_correction ? 0.5W^2 - 0.5h : 0.5W^2
+
+"""
+    simiterintegrals(W::Real, q_12::Real, h::Real, eps::Real; ito_correction=true, kwargs...)
+
+In the case of a scalar Q-Wiener process with (scalar) covariance Q the integral can be explicitly
+calculated as ``\\int_0^h\\int_0^sdW(t)dW(s) = \\frac{1}{2}W(h)^2 - \\frac{1}{2}hQ``.
+
+Note that, as in the multidimensional case, the parameter `q_12` denotes the square root of the covariance.
+
+The parameter `eps` (as well as all additional keyword arguments) has no effect but is available 
+to provide the same interface as the multidimensional version.
+"""
+simiterintegrals(W::Real, q_12::Real, h::Real, eps::Real; ito_correction=true, kwargs...) = ito_correction ? 0.5W^2 - 0.5*h*q_12^2 : 0.5W^2
